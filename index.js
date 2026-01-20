b6e9a6634a3900da853b71e9e278a831932fe9da
tnayem <tasdidnayem@gmail.com>
2026-01-20 23:34:04 +0600

    add payment getway api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const SSLCommerzPayment = require('sslcommerz-lts')
 const dotenv = require('dotenv');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -22,11 +23,15 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
+const store_id = process.env.STORE_ID
+const store_passwd = process.env.STORE_PASS
+const is_live = false //true for live, false for sandbox
 async function run() {
     try {
         await client.connect();
-        const parcelCollection = client.db('parcelDb').collection('parcels');
+        const db = client.db("parcelDb")
+        const parcelCollection = db.collection('parcels');
+        const orderCollection = db.collection('orders')
 
         // ✅ POST parcel
         app.post('/parcels', async (req, res) => {
@@ -40,9 +45,10 @@ async function run() {
         app.get('/parcels', async (req, res) => {
             try {
                 const email = req.query.email;
+                const id = req.params.id
                 let query = {}
                 if (email) {
-                    query = {created_by: email}
+                    query = { created_by: email }
                 }
 
                 const result = await parcelCollection
@@ -55,13 +61,83 @@ async function run() {
                 res.status(500).send({ message: 'Failed to load parcels' });
             }
         });
-
+        // Get spacific data using id 
+        app.get('/parcels/:id', async (req, res) => {
+            const id = req.params.id
+            const data = { _id: new ObjectId(id) }
+            const result = await parcelCollection.findOne(data)
+            res.send(result)
+        })
         // ✅ DELETE parcel
         app.delete('/parcels/:id', async (req, res) => {
             const id = req.params.id;
             const result = await parcelCollection.deleteOne({ _id: new ObjectId(id) });
             res.send(result);
         });
+        // Payment Method 
+        
+        app.post("/order", async (req, res) => {
+            const tran_id = new ObjectId().toString()
+            const productTrackingId = req.body.tracking_Id
+            const product = await parcelCollection.findOne({ tracking_Id: productTrackingId })
+            const data = {
+                total_amount: product?.totalPrice,
+                currency: 'BDT',
+                tran_id: tran_id, // use unique tran_id for each api call
+                success_url: `http://localhost:5000/payment/success/${tran_id}`,
+                fail_url: 'http://localhost:3030/fail',
+                cancel_url: 'http://localhost:3030/cancel',
+                ipn_url: 'http://localhost:3030/ipn',
+                shipping_method: 'Courier',
+                product_name: 'Computer.',
+                product_category: 'Electronic',
+                product_profile: 'general',
+                cus_name: 'Customer Name',
+                cus_email: 'customer@example.com',
+                cus_add1: product?.senderAddress,
+                cus_add2: 'Dhaka',
+                cus_city: 'Dhaka',
+                cus_state: 'Dhaka',
+                cus_postcode: '1000',
+                cus_country: 'Bangladesh',
+                cus_phone: '01711111111',
+                cus_fax: '01711111111',
+                ship_name: 'Customer Name',
+                ship_add1: 'Dhaka',
+                ship_add2: 'Dhaka',
+                ship_city: 'Dhaka',
+                ship_state: 'Dhaka',
+                ship_postcode: 1000,
+                ship_country: 'Bangladesh',
+            };
+
+            const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+            sslcz.init(data).then(apiResponse => {
+                // Redirect the user to payment gateway
+                let GatewayPageURL = apiResponse.GatewayPageURL
+                res.send({ url: GatewayPageURL })
+                const finalOrder = {
+                    product,
+                    paidStatus: false,
+                    transitationId: tran_id
+                }
+                const result = orderCollection.insertOne(finalOrder)
+                console.log('Redirecting to: ', GatewayPageURL)
+            });
+
+        })
+        app.post('/payment/success/:tranId', async (req, res) => {
+            console.log(req.params.tranId);
+            const result = await orderCollection.updateOne({ transitationId: req.params.tranId },
+                {
+                    $set: {
+                        paidStatus: true,
+                    }
+                })
+            if (result.modifiedCount > 0) {
+                res.redirect(`http://localhost:5173/payment/success/${req.params.tranId}`)
+            }
+        })
 
         console.log('MongoDB connected');
     } finally { }
